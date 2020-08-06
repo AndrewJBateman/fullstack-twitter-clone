ee7a7e39d40f3b373a4f507b349c17da3ccb0aef
Andrew Bateman <gomezbateman@yahoo.com>
2020-08-06 11:06:14 +0200

    server dependencies updated, tested OK

--- server/index.js
+++ server/index.js
@@ -15,46 +15,47 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.json({
-		message: 'tweeeeeettttt'
+		message: 'tweeeeeettttt',
 	});
 });
 
 app.get('/tweets', (req, res, next) => {
-	tweets
-		.find()
-		.then(tweets => {
-			res.json(tweets);
-		});
+	tweets.find().then((tweets) => {
+		res.json(tweets);
+	});
 });
 
 function isValidTweet(tweet) {
-	return tweet.name && tweet.name.toString().trim() !== '' && 
-	tweet.content && tweet.content.toString().trim() !== '';
+	return (
+		tweet.name &&
+		tweet.name.toString().trim() !== '' &&
+		tweet.content &&
+		tweet.content.toString().trim() !== ''
+	);
 }
 
-app.use(rateLimit({
-  	windowMs: 30 * 1000, // 30 seconds
-  	max: 1 // limit each IP to 1 request per windowMs
-}));
+app.use(
+	rateLimit({
+		windowMs: 30 * 1000, // 30 seconds
+		max: 1, // limit each IP to 1 request per windowMs
+	})
+);
 
 app.post('/tweets', (req, res, next) => {
-	if(isValidTweet(req.body)) {
+	if (isValidTweet(req.body)) {
 		const tweet = {
 			name: filter.clean(req.body.name.toString()),
 			content: filter.clean(req.body.content.toString()),
-			created: new Date()
+			created: new Date(),
 		};
 
-		tweets
-			.insert(tweet)
-			.then(createdTweet => {
-				res.json(createdTweet);
-			});
-
+		tweets.insert(tweet).then((createdTweet) => {
+			res.json(createdTweet);
+		});
 	} else {
 		res.status(422);
 		res.json({
-			message: 'Helooo, name and message are required'
+			message: 'Helooo, name and message are required',
 		});
 	}
 });
